7bb327ff4535a7999d8e7a984434b79a3c8bf727
MohitKumar <doddimohitkumar@gmail.com>
2024-08-27 11:41:15 +0530

    updates

---
+++ day1/variable.tf
@@ -0,0 +1,15 @@
+variable "ami" {
+    description = "value"
+    type = string 
+    default = "ami-02b49a24cfb95941c"
+}
+variable "instance_type" {
+    description = "value"
+    type = string
+    default = "t2.micro"
+}
+variable "key_name" {
+     description = "value"
+    type = string
+    default = "pair"
+}
