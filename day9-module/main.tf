1c369fe4d9bdf1d70b26de499e9b805d9f328989
MohitKumar <doddimohitkumar@gmail.com>
2024-09-02 16:14:49 +0530

    updates

--- day9-module/main.tf
+++ day9-module/main.tf
@@ -1,6 +1,11 @@
-module "dev" {
+module "mod" {
     source = "../day2"
     ami = "ami-066784287e358dad1"
-    instance_type ="t2.micro"
-    key_name ="keypair" 
+    instance_type = "t2.micro"
+    key_name = "keypair"
 }
+
+# module:
+#-take any other day as source and give that days ami, instance_type, key_name 
+#-after do terraform init,plan,appy 
+#-the instance will be created
